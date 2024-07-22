4953a5231c0b9980f50829deba3f825467d2a328
gitvipsystems <fabian@vip-systems.com.br>
2024-07-22 13:28:34 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-dd(__DIR__);
+
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
